3ff9a9d72549e97a1e5b2929f4067212084380f5
elvinciqueira <elvinciqueira@gmail.com>
2020-07-02 15:36:04 -0300

    feat: estilizando formulário

--- routes.js
+++ routes.js
@@ -2,11 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (request, response) => {
-  return response.redirect('/intructors')
+  return response.redirect('/instructors')
 })
 
-routes.get('/intructors', (request, response) => {
-  return response.render('Intructors/index')
+routes.get('/instructors', (request, response) => {
+  return response.render('Instructors/index')
+})
+
+routes.post('/instructors', (request, response) => {
+  return response.send('recebido')
+})
+
+routes.get('/instructors/create', (request, response) => {
+  return response.render('Instructors/create')
 })
 
 routes.get('/members', (request, response) => {
