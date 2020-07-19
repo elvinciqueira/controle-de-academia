59dce1b94c4ea9ed3bdec5137d61f531ccfa7309
elvinciqueira <elvinciqueira@gmail.com>
2020-07-19 18:02:52 -0300

    Algoritmo de paginação

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,40 @@
-const currentPage = location.pathname
-const menuItems = document.querySelectorAll('header .links a')
+(() => {
+  const currentPage = location.pathname;
+  const menuItems = document.querySelectorAll('header .links a');
 
-for (item of menuItems) {
-  if (currentPage.includes(item.getAttribute('href'))) {
-    item.classList.add('active')
+  for (item of menuItems) {
+    if (currentPage.includes(item.getAttribute('href'))) {
+      item.classList.add('active');
+    }
   }
-}
+
+  const pagination = (selectedPage, totalPages) => {
+    let pages = [],
+      oldPage;
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+      const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+      const pagesAfterSelectedPage = currentPage <= seletectedPage + 2;
+      const pagesBeforeSelectedPage = currentPage >= seletectedPage - 2;
+
+      if (
+        firstAndLastPage ||
+        (pagesBeforeSelectedPage && pagesAfterSelectedPage)
+      ) {
+        if (oldPage && currentPage - oldPage > 2) {
+          pages.push('...');
+        }
+
+        if (oldPage && currentPage - oldPage == 2) {
+          pages.push(oldPage + 1);
+        }
+
+        pages.push(currentPage);
+
+        oldPage = currentPage;
+      }
+    }
+
+    return pages;
+  };
+})();
